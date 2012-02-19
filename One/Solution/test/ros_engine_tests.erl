83b5a8170d610ed63c16b39768f6367cbe62581c
Gianfranco Alongi <zenon@zentop.mydomain.local>
2012-02-19 20:00:01 +0100

    Renamed from negative to ok test

--- One/Solution/test/ros_engine_tests.erl
+++ One/Solution/test/ros_engine_tests.erl
@@ -1,7 +1,7 @@
 -module(ros_engine_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-ros_engine_negative_test() ->
+ros_engine_ok_test() ->
     Dir = "../ROS_files/",
     Res = ros_engine:run_dir(Dir),
     ?assertEqual(
@@ -9,5 +9,4 @@ ros_engine_negative_test() ->
      {"february-south_branch-2011.ros",ok},
      {"january-north_branch-2011.ros",ok},
      {"january-south_branch-2011.ros",ok}],Res).
-     
-    
+
