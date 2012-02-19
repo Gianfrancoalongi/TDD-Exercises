d5822dbe00e0d9ee258c1e528f39813bed3a2c9e
Gianfranco Alongi <zenon@zentop.mydomain.local>
2012-02-19 20:11:00 +0100

    Added new negative test for ros_engine and modified failing test for ros_reader

--- One/Solution/test/ros_engine_tests.erl
+++ One/Solution/test/ros_engine_tests.erl
@@ -10,3 +10,10 @@ ros_engine_ok_test() ->
      {"january-north_branch-2011.ros",ok},
      {"january-south_branch-2011.ros",ok}],Res).
 
+ros_engine_negative_test() ->
+    Dir = "test/ros_files/",
+    Res = ros_engine:run_dir(Dir),
+    ?assertEqual(
+    [{"file_a.ros",{error,no_total}},
+     {"file_b.ros",{error,no_total}}],Res).
+    
--- One/Solution/test/ros_reader_tests.erl
+++ One/Solution/test/ros_reader_tests.erl
@@ -3,7 +3,7 @@
 
 ros_reads_file_test() ->
     File = "./test/january-good-2011.ros",
-    ?assertMatch({ok,Result},ros_reader:read_file(File)).
+    ?assertEqual(<<>>,ros_reader:read_file(File)).
 
 ros_read_bad_file_test() ->
     File = "./test/nonexistent.ros",
