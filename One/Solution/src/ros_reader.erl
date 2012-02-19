ac5700f6382c617aa90109ccdf31b75b562933d5
Gianfranco Alongi <zenon@zentop.local>
2012-02-19 15:42:30 +0100

    Added new files in test/ros_files/ for testing of directory read. Changes Makefile target with automatic init:stop() Added read directory test and imeplementation.

--- One/Solution/src/ros_reader.erl
+++ One/Solution/src/ros_reader.erl
@@ -1,11 +1,21 @@
 -module(ros_reader).
--export([read_file/1]).
+-export([read_file/1,
+	 read_dir/1
+	]).
 
--spec(read_file(string()) -> {ok,term()}).
+-spec(read_file(string()) -> {ok,term()} | {error,no_such_file}).
 read_file(File) ->
     case file:read_file(File) of
 	{error,enoent} ->
 	    {error,no_such_file};
 	R -> R
+    end.
+
+-spec(read_dir(string()) -> {ok,[{string(),term()}]}).
+read_dir(Dir) ->
+    case file:list_dir(Dir) of
+	{ok,Files} ->
+	    Res = [{File, read_file(filename:join(Dir,File))} || File <- Files ],	    
+	    {ok,lists:sort(Res)}
     end.
 
--- One/Solution/test/ros_reader_tests.erl
+++ One/Solution/test/ros_reader_tests.erl
@@ -8,5 +8,12 @@ ros_reads_file_test() ->
 ros_read_bad_file_test() ->
     File = "./test/nonexistent.ros",
     ?assertMatch({error,no_such_file},ros_reader:read_file(File)).
+
+ros_read_dir_test() ->
+    Dir = "./test/ros_files/",
+    ?assertMatch({ok,
+		  [{"file_a.ros",ResultA},
+		   {"file_b.ros",ResultB}]},
+		 ros_reader:read_dir(Dir)).
     
     
