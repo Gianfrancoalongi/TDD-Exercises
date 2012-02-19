ce093af87b38b02cd4e6837de9f73d34bd9a83ef
Gianfranco Alongi <zenon@zentop.local>
2012-02-19 15:32:51 +0100

    Added negative test for nonexistent files for ros_reader

--- One/Solution/src/ros_reader.erl
+++ One/Solution/src/ros_reader.erl
@@ -3,5 +3,9 @@
 
 -spec(read_file(string()) -> {ok,term()}).
 read_file(File) ->
-    file:read_file(File).
+    case file:read_file(File) of
+	{error,enoent} ->
+	    {error,no_such_file};
+	R -> R
+    end.
 
--- One/Solution/test/ros_reader_tests.erl
+++ One/Solution/test/ros_reader_tests.erl
@@ -4,4 +4,9 @@
 ros_reads_file_test() ->
     File = "./test/january-good-2011.ros",
     ?assertMatch({ok,Result},ros_reader:read_file(File)).
+
+ros_read_bad_file_test() ->
+    File = "./test/nonexistent.ros",
+    ?assertMatch({error,no_such_file},ros_reader:read_file(File)).
+    
     
